21769fe1139382eab7e2cc66900ae7b02c5c6712
Samuel Costanzo <costanzodev25@gmail.com>
2026-08-18 20:06:13 -0400

    Test

--- resources/js/components/MovieGrid.tsx
+++ resources/js/components/MovieGrid.tsx
@@ -3,10 +3,13 @@ import MovieCard from './MovieCard';
 
 export default function MovieGrid({ movies }: { movies: Movie[] }) {
     return (
-        <div className="grid grid-cols-2 gap-4 md:grid-cols-4 lg:grid-cols-6">
-            {movies.map((movie) => (
-                <MovieCard key={movie.id} movie={movie} />
-            ))}
-        </div>
+        <>
+            <h1>The Movie Grid!</h1>
+            <div className="grid grid-cols-2 gap-4 md:grid-cols-4 lg:grid-cols-6">
+                {movies.map((movie) => (
+                    <MovieCard key={movie.id} movie={movie} />
+                ))}
+            </div>
+        </>
     );
 }
